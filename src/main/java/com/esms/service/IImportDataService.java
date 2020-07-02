e6878b8af79e863c92e558e0008c6cf6eff5cce0
Voper <1984758277@qq.com>
2020-07-02 12:03:12 +0800

    初始版本

--- src/main/java/com/esms/service/IImportDataService.java
+++
@@ -1,8 +0,0 @@
-package com.esms.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface IImportDataService {
-    String insertMATable(MultipartFile excel) throws Exception;
-    String insertReissueTable(MultipartFile excel) throws Exception;
-}
